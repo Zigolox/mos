fd353d3439f2abd6d79b4fbc402d713e363e2eee
Zigolox <41542666+Zigolox@users.noreply.github.com>
2024-02-02 11:21:01 +0100

    changed logging

--- mos/log.py
+++ mos/log.py
@@ -1,11 +1,13 @@
 """Module responsible for logging the training process using Weights and Biases."""
-import wandb
-from eval import evaluate
-from jaxtyping import PRNGKeyArray, Array
 from typing import Callable
-from dataset import AudioDataset
-from models import DeepMos
+
 import equinox as eqx
+from dataset import AudioDataset
+from eval import evaluate
+from jaxtyping import Array, PRNGKeyArray
+
+import wandb
+from models import DeepMos
 
 
 def log_eval(
@@ -31,8 +33,9 @@ def log_multiple(array: Array, tag: str, name: str) -> None:
 def save_model(model, model_state, epoch):
     """Saves the model to wandb"""
     model_file_name = f"models/{model.__class__.__name__}_gstep{epoch}.eqx"
-    model = eqx.nn.inference_mode(model)
-    model = eqx.Partial(model, state=model_state)
+    state_file_name = f"models/{model.__class__.__name__}_state_gstep{epoch}.eqx"
 
     eqx.tree_serialise_leaves(model_file_name, model)
+    eqx.tree_serialise_leaves(state_file_name, model_state)
     wandb.save(model_file_name)
+    wandb.save(state_file_name)
